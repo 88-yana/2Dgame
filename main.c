ba9fcbfe4740dcb8910bf3379c97c8b06e710611
Hayato Yanagimura <hyanagim@c1r17s2.42tokyo.jp>
2022-08-30 18:04:26 +0900

    draw images

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 19:13:02 by hyanagim          #+#    #+#             */
-/*   Updated: 2022/08/30 17:20:33 by hyanagim         ###   ########.fr       */
+/*   Updated: 2022/08/30 18:01:30 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,12 @@ int main()
 	int height = 1000;
 	int w = 0;
 	int h = 0;
-	char *image = "./stop.xpm";
+	char *image = "./tree.xpm";
+	char *image1 = "./cherry.xpm";
+	char *image2 = "./door.xpm";
 	void *image_ptr;
+	void *image_ptr1;
+	void *image_ptr2;
 	mlx = mlx_init();//fileopenの時のfd的な？
 	mlx_win = mlx_new_window(mlx, width, height,"so_long");
 	// mlx_win = mlx_new_window(スクリーン識別子, width, height,"test");
@@ -56,7 +60,15 @@ int main()
 			i++;
 	}
 	image_ptr = mlx_xpm_file_to_image(mlx, image, &w, &h);
-	mlx_put_image_to_window(mlx, mlx_win, image_ptr, 0, 0);
+	image_ptr1 = mlx_xpm_file_to_image(mlx, image1, &w, &h);
+	image_ptr2 = mlx_xpm_file_to_image(mlx, image2, &w, &h);
+	for (int i = 0; i < 3; i++)
+	{
+		mlx_put_image_to_window(mlx, mlx_win, image_ptr, 100 * i, 0);
+	}
+	
+	mlx_put_image_to_window(mlx, mlx_win, image_ptr1, 100, 100);
+	mlx_put_image_to_window(mlx, mlx_win, image_ptr2, 200, 200);
 	mlx_loop(mlx);//無限ループを作り、その間ウィンドウを表示させ続ける
 	//最初のうちはcntl+Cで強制終了すればおk
 	//クリックした時に終了させるなどのプログラムはゆくゆく書く必要がある
